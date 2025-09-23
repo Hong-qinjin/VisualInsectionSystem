004d639db16ff587f4dd819fc90fff506d894157
S6543 <S6543@PCS34SQ0012>
2025-09-23 13:01:48 +0800

    添加项目文件。

---
+++ Systemdemo01/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Systemdemo01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.vmProcedureConfigControl1 = new VMControls.Winform.Release.VmProcedureConfigControl();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.vmRenderControl1 = new VMControls.Winform.Release.VmRenderControl();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.vmParamsConfigWithRenderControl1 = new VMControls.Winform.Release.VmParamsConfigWithRenderControl();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.vmParamsConfigControl1 = new VMControls.Winform.Release.VmParamsConfigControl();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.vmProcedureConfigControl1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 25);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(419, 468);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter_1);
+            // 
+            // vmProcedureConfigControl1
+            // 
+            this.vmProcedureConfigControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vmProcedureConfigControl1.Location = new System.Drawing.Point(2, 16);
+            this.vmProcedureConfigControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.vmProcedureConfigControl1.Name = "vmProcedureConfigControl1";
+            this.vmProcedureConfigControl1.Size = new System.Drawing.Size(415, 450);
+            this.vmProcedureConfigControl1.TabIndex = 0;
+// TODO: “”的代码生成失败，原因是出现异常“无效的基元类型: System.IntPtr。请考虑使用 CodeObjectCreateExpression。”。
+            this.vmProcedureConfigControl1.Load += new System.EventHandler(this.vmProcedureConfigControl1_Load_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(9, 495);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(417, 132);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.textBox2.Location = new System.Drawing.Point(66, 101);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(344, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.textBox1.Location = new System.Drawing.Point(66, 24);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.button6.Location = new System.Drawing.Point(4, 101);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(56, 22);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "保存";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.button5.Location = new System.Drawing.Point(127, 74);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 22);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "停止";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.button4.Location = new System.Drawing.Point(66, 74);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 22);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "连续";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.button3.Location = new System.Drawing.Point(4, 74);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 22);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "单次";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.button2.Location = new System.Drawing.Point(4, 46);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "加载";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.button1.Location = new System.Drawing.Point(4, 19);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Location = new System.Drawing.Point(430, 496);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(320, 131);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("宋体", 9.25F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(2, 16);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(316, 113);
+            this.listBox1.TabIndex = 0;
+            // 
+            // vmRenderControl1
+            // 
+            this.vmRenderControl1.BackColor = System.Drawing.Color.Black;
+            this.vmRenderControl1.CoordinateInfoVisible = true;
+            this.vmRenderControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vmRenderControl1.ImageSource = null;
+            this.vmRenderControl1.Location = new System.Drawing.Point(3, 17);
+            this.vmRenderControl1.ModuleSource = null;
+            this.vmRenderControl1.Name = "vmRenderControl1";
+            this.vmRenderControl1.Size = new System.Drawing.Size(314, 448);
+            this.vmRenderControl1.TabIndex = 3;
+            this.vmRenderControl1.Load += new System.EventHandler(this.vmRenderControl1_Load);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.vmRenderControl1);
+            this.groupBox4.Location = new System.Drawing.Point(430, 25);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(320, 468);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox4";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.vmParamsConfigWithRenderControl1);
+            this.groupBox5.Location = new System.Drawing.Point(753, 25);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(541, 378);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "groupBox5";
+            this.groupBox5.Enter += new System.EventHandler(this.groupBox5_Enter);
+            // 
+            // vmParamsConfigWithRenderControl1
+            // 
+            this.vmParamsConfigWithRenderControl1.BackColor = System.Drawing.Color.Black;
+            this.vmParamsConfigWithRenderControl1.CoordinateInfoVisible = true;
+            this.vmParamsConfigWithRenderControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vmParamsConfigWithRenderControl1.ImageSource = null;
+            this.vmParamsConfigWithRenderControl1.Location = new System.Drawing.Point(3, 17);
+            this.vmParamsConfigWithRenderControl1.ModuleSource = null;
+            this.vmParamsConfigWithRenderControl1.MultiImageButtonVisible = false;
+            this.vmParamsConfigWithRenderControl1.Name = "vmParamsConfigWithRenderControl1";
+            this.vmParamsConfigWithRenderControl1.ParamsConfig = null;
+            this.vmParamsConfigWithRenderControl1.ROIVisible = true;
+            this.vmParamsConfigWithRenderControl1.Size = new System.Drawing.Size(535, 358);
+            this.vmParamsConfigWithRenderControl1.TabIndex = 0;
+            this.vmParamsConfigWithRenderControl1.Load += new System.EventHandler(this.vmParamsConfigWithRenderControl1_Load);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.vmParamsConfigControl1);
+            this.groupBox6.Location = new System.Drawing.Point(753, 404);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(541, 223);
+            this.groupBox6.TabIndex = 5;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "groupBox6";
+            this.groupBox6.Enter += new System.EventHandler(this.groupBox6_Enter);
+            // 
+            // vmParamsConfigControl1
+            // 
+            this.vmParamsConfigControl1.BackColor = System.Drawing.Color.Silver;
+            this.vmParamsConfigControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vmParamsConfigControl1.Location = new System.Drawing.Point(3, 17);
+            this.vmParamsConfigControl1.ModuleSource = null;
+            this.vmParamsConfigControl1.Name = "vmParamsConfigControl1";
+            this.vmParamsConfigControl1.ParamsConfig = null;
+            this.vmParamsConfigControl1.Size = new System.Drawing.Size(535, 203);
+            this.vmParamsConfigControl1.TabIndex = 0;
+            this.vmParamsConfigControl1.Load += new System.EventHandler(this.vmParamsConfigControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1306, 640);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "HikonDemo1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private VMControls.Winform.Release.VmProcedureConfigControl vmProcedureConfigControl1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private VMControls.Winform.Release.VmRenderControl vmRenderControl1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private VMControls.Winform.Release.VmParamsConfigWithRenderControl vmParamsConfigWithRenderControl1;
+        private VMControls.Winform.Release.VmParamsConfigControl vmParamsConfigControl1;
+    }
+}
+
---
+++ Systemdemo01/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using VM.Core;
+using VM.PlatformSDKCS;
+using VMControls.BaseInterface;
+using VMControls.Winform.Release;
+
+/// <summary>
+/// 系统Demo01-02方案加载与显示，执行，保存界面
+/// </summary>  
+
+namespace Systemdemo01
+{
+    /// <summary>
+    /// 方案加载与显示界面
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void vmProcedureConfigControl1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox2_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 选择路径按钮功能，打开一个文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = null;
+            try
+            {
+                // 调试输出按钮点击
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = "E:\\VisualSoftware\\ABVisualSystem\\";
+                //openFileDialog.Title = "选择VM Sol文件";
+                //openFileDialog.Filter = "All files (*.*)|*.*";  //设置文件类型
+                openFileDialog.Filter = "VM Sol File|*.sol";
+                DialogResult openFileRes = openFileDialog.ShowDialog(); //显示打开文件对话框
+                                                                        // 调试输出对话框结果
+                System.Diagnostics.Debug.WriteLine($"对话框结果: {openFileRes}");
+
+                if (openFileRes == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog.FileName; 
+
+                    // 调试输出获取的路径
+                    System.Diagnostics.Debug.WriteLine($"选中的文件路径: {textBox1.Text}");
+                }
+
+                listBox1.Items.Add("选择路径成功");
+                listBox1.TopIndex = listBox1.Items.Count - 1;   //选择路径成功，显示在listbox中1.2_1:50
+            }
+            catch (VmException ex)
+            {
+                message = "Load Sol Failed." + ex.errorCode;
+                listBox1.Items.Add(message);
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+            }
+
+        }
+        /// <summary>
+        /// 方案加载按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VmSolution.Load(textBox1.Text);   //// 引用VM.Solution.load方法选中路径的sol文件,1.2_1:54
+                listBox1.Items.Add("方案加载成功" + textBox1.Text);
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+            }
+            catch (VmException ex)
+            {
+                string message = "Clear Sol Failed." + ex.errorCode;
+                listBox1.Items.Add(message);
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                string message = "File Not Found." + ex.Message;
+                listBox1.Items.Add(message);
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+            }
+
+
+
+        /// <summary>
+        /// 执行一次按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //1.4模块的参数配置
+                //IMVSOcrModuCs.IMVSOcrModuTool ocrTool = (IMVSOcrModuCs.IMVSOcrModuTool)VmSolution.Instance[""];
+                IMVSOcrDlModuCCs.IMVSOcrDlModuCTool ocrTool = (IMVSOcrDlModuCCs.IMVSOcrDlModuCTool)VmSolution.Instance["OCRDemo.DL字符识别C1"];  //获取方案中的模块对象1.4_1:20  
+                vmParamsConfigWithRenderControl1.ModuleSource = ocrTool;  //带渲染的参数控件与模块绑定1.4_1:24
+
+                vmParamsConfigControl1.ModuleSource = ocrTool;  //参数控件与模块绑定1.4_2:39
+            }
+            catch(VmException ex)
+            {
+                string message = "参数配置页面失败." + ex.errorCode;
+                listBox1.Items.Add(message);
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+            }
+
+
+            VmSolution.Instance.SyncRun();   //同步执行一次方案中所有流程1.2_2:07
+            listBox1.Items.Add("单次执行成功");
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+
+            #region 
+
+            // 1.3获取结果,分为渲染结果以及数据结果
+            VMControls.Winform.Release.VmRenderControl vmRenderControl = new VMControls.Winform.Release.VmRenderControl();
+            //VmProcedure vmProcedure = (VmProcedure)VmSolution.Instance[""];   //流程对象 
+            VmProcedure vmProcedure = (VmProcedure)VmSolution.Instance["OCRDemo"];   //流程对象            
+            
+            vmRenderControl1.ModuleSource = vmProcedure;  //设置渲染控件的显示源与流程绑定
+            vmRenderControl.Show();  //显示渲染控件                                       
+            string ocrResult = vmProcedure.ModuResult.GetOutputString("out").astStringVal[0].strValue;  //   获取识别结果
+            string ocrConfidence = vmProcedure.ModuResult.GetOutputString("out0").astStringVal[0].strValue;  //  获取置信度
+            string ocrNum = vmProcedure.ModuResult.GetOutputInt("out1").pIntVal[0].ToString();  //  数量
+
+            listBox1.Items.Add("字符识别结果:" + ocrResult);
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+            listBox1.Items.Add("字符置信度:" + ocrConfidence);
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+            listBox1.Items.Add("字符数量:" + ocrNum);
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+
+
+            #endregion
+
+        }
+        /// <summary>
+        /// 连续执行按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Add("连续执行中");
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+
+        }
+
+        /// <summary>
+        /// 停止
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Add("停止.");
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+
+        }
+        /// <summary>
+        /// 保存按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            VmSolution.Save();   // VM.Solution.save方法,保存方案到原始路径并替换原有文件1.2_2:16
+            //VmSolution.SaveAs("path");   //保存方案到自定义路径
+
+            ////检测硬盘大小是否足够保存文件
+            ///VmSolution.Instance.SaveFailed = false;   //假设硬盘空间足够保存文件 
+            //if (VmSolution.Instance.SaveFailed)
+            //{
+            //    MessageBox.Show("硬盘空间保存失败！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //}
+            //else
+            //{
+            //    //文件保存成功提示
+            //    MessageBox.Show("文件保存成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //}                
+            listBox1.Items.Add("保存成功.");
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 渲染控件加载,图像显示控件
+        /// 1.3渲染以及结果获取
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void vmRenderControl1_Load(object sender, EventArgs e)
+        {
+            //VMControls.Winform.Release.VmRenderControl vmRenderControl = new VMControls.Winform.Release.VmRenderControl();
+        }
+
+        private void groupBox5_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox6_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vmParamsConfigWithRenderControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vmParamsConfigControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
